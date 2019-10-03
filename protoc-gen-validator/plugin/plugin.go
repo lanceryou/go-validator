2bdd72966d8f095ac0490b37cd04188c6a96e359
Chihong You <chihong.you@shopee.com>
2019-10-03 13:20:15 +0800

    commit validator

--- protoc-gen-validator/plugin/plugin.go
+++ protoc-gen-validator/plugin/plugin.go
@@ -1,6 +1,7 @@
 package plugin
 
 import (
+	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/golang/protobuf/protoc-gen-go/generator"
@@ -42,23 +43,19 @@ func (v *validator) GenerateImports(file *generator.FileDescriptor) {}
 func (g *validator) P(args ...interface{}) { g.gen.P(args...) }
 
 func (v *validator) generateProto3Validator(file *generator.FileDescriptor, desc *descriptor.DescriptorProto) {
-	v.P("// generate field.")
-	for _, field := range desc.Field {
-		v.P(field.Name, ",type ", field.Type.String(), "type name ", field.GetTypeName(), "value ", field.GetDefaultValue(), "extend", field.GetExtendee())
-	}
-	v.P("// generate extend.")
-	v.P()
-	for _, field := range desc.Extension {
-		v.P(field.Name, ",extend type ", field.Type.String(), "type name ", field.GetTypeName(), "value ", field.GetDefaultValue())
+	if !hasValidatorField(desc) {
+		return
 	}
 
 	ccTypeName := generator.CamelCase(desc.GetName())
 	v.P(`func (this *`, ccTypeName, `) Validate() error {`)
+	v.gen.In()
 	// support nested message
 	for _, field := range desc.Field {
-		v.generateField(file, field)
+		v.generateField(ccTypeName, file, field, desc)
 	}
 	v.P(`return nil`)
+	v.gen.Out()
 	v.P(`}`)
 	v.P()
 }
@@ -66,12 +63,128 @@ func (v *validator) generateProto3Validator(file *generator.FileDescriptor, desc
 // number string 类型 按照type比较
 // message 类型 生成
 // reqeated 类型判断长度
-func (v *validator) generateField(file *generator.FileDescriptor, field *descriptor.FieldDescriptorProto) {
+func (v *validator) generateField(ccTypeName string, file *generator.FileDescriptor, field *descriptor.FieldDescriptorProto, desc *descriptor.DescriptorProto) {
 	fieldValidator := getValidatorField(field)
 	if fieldValidator == nil {
 		return
 	}
 
+	if field.Type == nil {
+		return
+	}
+
+	variableName := "this." + generator.CamelCase(*field.Name)
+	if isMessage(field) && !isRepeated(field) {
+		// validate nil and validate message if not nil
+		v.generateMessageValidator(variableName, ccTypeName, fieldValidator, desc)
+	} else if isRepeated(field) || isString(field) {
+		// validate length
+		v.generateArrayValidator(variableName, ccTypeName, fieldValidator)
+	} else {
+		v.generateFieldValidator(variableName, ccTypeName, fieldValidator)
+	}
+}
+
+func (v *validator) generateMessageValidator(variableName string, ccTypeName string, fv *valid.FieldValidator, desc *descriptor.DescriptorProto) {
+	if fv.Neq == "nil" {
+		v.P(`if !(`, variableName, `!=`, fv.Neq, `) {`)
+		v.gen.In()
+		v.P(`return fmt.Errorf("validation error: `, variableName, ` must be not equal nil")`)
+		v.gen.Out()
+		v.P(`}`)
+	}
+
+	// if err := variableName.Validate(); err != nil{
+	// 		return err
+	// }
+	/*v.P(`if err := `, variableName, `.Validate(); err != nil{`)
+	v.gen.In()
+	v.P(`return err`)
+	v.gen.Out()
+	v.P(`}`)*/
+}
+
+func (v *validator) generateArrayValidator(variableName string, ccTypeName string, fv *valid.FieldValidator) {
+	type Filed struct {
+		Opt   string
+		Value string
+		Err   string
+	}
+
+	fields := []Filed{
+		{
+			Opt:   " < ",
+			Value: fv.Lt,
+			Err:   fmt.Sprintf("%s be greater than len(%s)", variableName, fv.Lt),
+		},
+		{
+			Opt:   " > ",
+			Value: fv.Gt,
+			Err:   fmt.Sprintf("%s be less than len(%s)", variableName, fv.Gt),
+		},
+		{
+			Opt:   " == ",
+			Value: fv.Eq,
+			Err:   fmt.Sprintf("%s be not equal len(%s)", variableName, fv.Eq),
+		},
+		{
+			Opt:   " != ",
+			Value: fv.Neq,
+			Err:   fmt.Sprintf("%s be equal len(%s)", variableName, fv.Neq),
+		},
+	}
+
+	for _, field := range fields {
+		if field.Value != "" {
+			v.P(`if !(len( `, variableName, `)`, field.Opt, field.Value, `) {`)
+			v.gen.In()
+			v.P(`return fmt.Errorf("validation error: `, field.Err, `")`)
+			v.gen.Out()
+			v.P(`}`)
+			v.gen.Out()
+		}
+	}
+}
+
+func (v *validator) generateFieldValidator(variableName string, ccTypeName string, fv *valid.FieldValidator) {
+	type Filed struct {
+		Opt   string
+		Value string
+		Err   string
+	}
+
+	fields := []Filed{
+		{
+			Opt:   " < ",
+			Value: fv.Lt,
+			Err:   fmt.Sprintf("%s be greater than %s", variableName, fv.Lt),
+		},
+		{
+			Opt:   " > ",
+			Value: fv.Gt,
+			Err:   fmt.Sprintf("%s be less than %s", variableName, fv.Gt),
+		},
+		{
+			Opt:   " == ",
+			Value: fv.Eq,
+			Err:   fmt.Sprintf("%s be not equal %s", variableName, fv.Eq),
+		},
+		{
+			Opt:   " != ",
+			Value: fv.Neq,
+			Err:   fmt.Sprintf("%s be equal %s", variableName, fv.Gt),
+		},
+	}
+
+	for _, field := range fields {
+		if field.Value != "" {
+			v.P(`if !(`, variableName, field.Opt, field.Value, `) {`)
+			v.gen.In()
+			v.P(`return fmt.Errorf("validation error: `, field.Err, `")`)
+			v.gen.Out()
+			v.P(`}`)
+		}
+	}
 }
 
 func getValidatorField(field *descriptor.FieldDescriptorProto) *valid.FieldValidator {
@@ -85,4 +198,26 @@ func getValidatorField(field *descriptor.FieldDescriptorProto) *valid.FieldValid
 	}
 
 	return nil
+}
+
+func hasValidatorField(desc *descriptor.DescriptorProto) (has bool) {
+	for _, field := range desc.Field {
+		if field.Options != nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isRepeated(field *descriptor.FieldDescriptorProto) bool {
+	return field.Label != nil && *field.Label == descriptor.FieldDescriptorProto_LABEL_REPEATED
+}
+
+func isMessage(field *descriptor.FieldDescriptorProto) bool {
+	return *field.Type == descriptor.FieldDescriptorProto_TYPE_MESSAGE
+}
+
+func isString(field *descriptor.FieldDescriptorProto) bool {
+	return *field.Type == descriptor.FieldDescriptorProto_TYPE_STRING
 }
